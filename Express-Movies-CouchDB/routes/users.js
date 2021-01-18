028e4ab5aa655a34eba357898c4b59f9fbc206a4
jacoFixSmile <61232800+jacoFixSmile@users.noreply.github.com>
2021-01-18 09:32:44 +0100

    Nieuw begin
    express opnieuw aangemaakt met correcte libs

--- Express-Movies-CouchDB/routes/users.js
+++ Express-Movies-CouchDB/routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/add', function(req, res, next) {
+  res.render('add.ejs', {});
+});
 module.exports = router;

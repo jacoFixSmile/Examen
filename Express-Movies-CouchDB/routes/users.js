152ef483e71d2f7ab9f2a15355a154a25eae9042
jacoFixSmile <61232800+jacoFixSmile@users.noreply.github.com>
2021-01-18 09:51:45 +0100

    list semi werkende
    veel opnieuw begonnen en list werkt half

--- Express-Movies-CouchDB/routes/users.js
+++ Express-Movies-CouchDB/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios').default;
+const DB_URL = "http://127.0.0.1:5984/movies/";
+const DB_VIEWS = "_design/views/_view/";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  axios.get(DB_URL + DB_VIEWS + 'allMovies')
+  .then(function (response) {
+    //console.log(response.data.rows);
+    res.render('list.ejs', { products: response.data.rows });
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 });
 router.get('/add', function(req, res, next) {
   res.render('add.ejs', {});
